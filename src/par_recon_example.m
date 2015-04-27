8726d1221cfd92178119a1adaa4a5fc15134771a
BG <brendhanpgivens@BRENDHANs-iMac.local>
2015-04-26 20:46:19 -0400

    Working
    Produces noiseless sonogram and reconstructs it.

--- src/par_recon_example.m
+++ src/par_recon_example.m
@@ -1,9 +1,14 @@
 %% Parallel Beam Reconstruction Example
-% Generate phantom, geometries, and produce a projection.
-% Reconstruct assuming fbp, or something else?
+% Generate phantom, generic geometries, and produce a projection.
+% Perform simple iterative reconstruction.
 
-% Phantom
-x = phantom(128);
+close all;
+
+%% Phantom
+x = phantom(128,'Shepp-Logan');
+
+%% Geometries
+% Image and sinogram geometries
 
 % Image geometry
 nx = 128;
@@ -22,9 +27,9 @@ ig = image_geom('nx',nx,'ny',ny, ...
 type = 'par';
 orbit_start = 90;
 orbit = 180;
-nb = 185; % number of bins
-na = 180; % number of angles
-dr = 1; % ray spacing
+nb = 280; % number of bins
+na = 240; % number of angles
+dr = .5; % ray spacing
 %offset_r = -.5; % channel offset
 offset_r = 0; % channel offset
 strip_width = dr; % strip width
@@ -33,13 +38,36 @@ sgr = sino_geom(type,...
     'nb',nb,'na',na,'dr',dr, ...
     'offset_r',0,'strip_width',strip_width);
 
-% Generate sinogram
-table = {'square/strip','Ltab',[1000],'strip_width',[dr]};
-nthread = jf('ncore');
-Gr = Gtomo2_table(sgr,ig,table,'nthread',nthread);
-rx = Gr*x;
+%% Generate sinogram
+table = {'square/strip','Ltab',[1000],'strip_width',[dr]}; % strip integral
+nthread = jf('ncore'); % number of threads in my computer
+G = Gtomo2_table(sgr,ig,table,'nthread',nthread);
+y = G*x;
 
-im(rx);
+%% Display phantom and sinogram
+figure; im(x,[0 .2]);
+figure; im(y);
 
-geom = fbp2(sgr, ig);
-xhat = fbp2(rx, geom);
+%% Initialize
+o = ones(128);
+o2 = G'*(G*o);
+m = mean(o2(:));
+x0 = (1/m)*G'*y;
+
+%% Iterative reconstruction with SD
+%alpha = 6e-5;
+N = 50;
+xhat = x0;
+for k = 1:N
+    r = y-G*xhat;
+    gr = G'*r;
+    alpha = (r(:)'*r(:))/(gr(:)'*gr(:));
+    xhat = xhat + alpha*G'*r;
+end
+
+err = x - xhat;
+
+% Display recon and error
+%figure; im(xhat);
+figure; im(xhat,[0 .2]);
+figure; im(err,[-.1 .1]); 
